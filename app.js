e9f2806ba0dde5d4c0557b043f6fbba97f305f8a
Yongjun <91wns11@naver.com>
2015-02-05 16:54:27 +0900

    KYJ _ login and join

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
-var join = require('./routes/join');
 
 var app = express();
 
